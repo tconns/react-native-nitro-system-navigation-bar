127de3bd9211a56a43556a70c18e1dfa5be3796f
tconns <thanhcongns94@gmail.com>
2025-08-23 23:38:56 +0700

    fix: interface test

--- ios/NitroSystemNavigationBar.swift
+++ ios/NitroSystemNavigationBar.swift
@@ -1,4 +1,32 @@
 class NitroSystemNavigationBar: HybridNitroSystemNavigationBarSpec {
+    func getNavigationBarHeight() throws -> Double {
+        return 0
+    }
+
+    func hideNavigationBar() throws {
+        
+    }
+
+    func showNavigationBar() throws {
+        
+    }
+
+    func enableImmersiveSticky() throws {
+        
+    }
+
+    func exitImmersiveMode() throws {
+        
+    }
+
+    func setNavigationBarColor(color: Double, isTranslucent: Bool?) throws {
+        
+    }
+
+    func setLightNavigationBar(isLight: Bool) throws {
+        
+    }
+
     public func multiply(a: Double, b: Double) throws -> Double {
         return a * b
     }
